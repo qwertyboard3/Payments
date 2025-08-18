af8056467106fadd9f4281134d58cbcfdaed5ce9
qwertyboard3 <qwertyboard_2@hotmail.com>
2025-08-18 10:48:35 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,35 @@
 const CACHE_NAME = 'pagos-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/manifest.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([
-      '/',
-      '/index.html',
-    ]))
+    caches.open(CACHE_NAME).then(cache =>
+      Promise.all(
+        FILES_TO_CACHE.map(file =>
+          cache.add(file).catch(err => console.warn('Failed to cache:', file, err))
+        )
+      )
+    )
   );
+  self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
     )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
